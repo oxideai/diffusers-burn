299e8cecd788b439e5401b48d9f2bed45882a50b
dcvz <david@dcvz.io>
2023-11-20 17:44:42 +0100

    Allow compiling

--- src/pipelines/stable_diffusion.rs
+++ src/pipelines/stable_diffusion.rs
@@ -5,7 +5,7 @@ pub struct StableDiffusionConfig {
     pub width: i64,
     pub height: i64,
     pub clip: clip::Config,
-    autoencoder: vae::AutoEncoderKLConfig,
-    unet: unet_2d::UNet2DConditionModelConfig,
-    scheduler: ddim::DDIMSchedulerConfig,
+    // autoencoder: vae::AutoEncoderKLConfig,
+    // unet: unet_2d::UNet2DConditionModelConfig,
+    // scheduler: ddim::DDIMSchedulerConfig,
 }
